cacf97c39829c82b5e9ee62d5d2d64ba40277c96
RisingPaw <pawn.risings.0o@icloud.com>
2022-05-24 22:38:22 +0200

    chore(config): fix BASE env var

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 // export const SITE = "https://evmavericks.github.io";
 // export const BASE = "/website-draft-RisingPaw";
 export const SITE = "https://dao.evmavericks.xyz/";
-export const BASE = "/";
+export const BASE = "";
 export const { PROD: IS_PROD } = import.meta.env;
